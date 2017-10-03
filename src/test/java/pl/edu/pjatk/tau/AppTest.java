c458ffa9851d48d82c72222320a7887e017d90a9
Dawid Richert <dawidrichert@gmail.com>
2017-10-03 20:05:27 +0200

    Reformat code

--- src/test/java/pl/edu/pjatk/tau/AppTest.java
+++ src/test/java/pl/edu/pjatk/tau/AppTest.java
@@ -6,18 +6,15 @@ import junit.framework.TestSuite;
 
 public class AppTest extends TestCase
 {
-    public AppTest(String testName)
-    {
+    public AppTest(String testName) {
         super(testName);
     }
 
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite(AppTest.class);
     }
 
-    public void testApp()
-    {
+    public void testApp() {
         assertTrue(true);
     }
 }
